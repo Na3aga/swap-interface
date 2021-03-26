410fc52522d4c7f5c77ebc796a71ab2dcdf8ab61
Nazar <nazar_h@magetic.com>
2021-03-26 13:14:07 +0200

    uniswap exchange deployment

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,114 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: 'http://localhost/',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    initialOpenState: true,
+    items: [
+      {
+        label: 'Exchange',
+        href: '/swap',
+      },
+      {
+        label: 'Liquidity',
+        href: '/pool',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: 'http://localhost/farms',
+  },
+  {
+    label: 'Pools',
+    icon: 'PoolIcon',
+    href: 'http://localhost/syrup',
+  },
+  {
+    label: 'Lottery',
+    icon: 'TicketIcon',
+    href: 'http://localhost/lottery',
+  },
+  {
+    label: 'NFT',
+    icon: 'NftIcon',
+    href: 'http://localhost/nft',
+  },
+  {
+    label: 'Teams & Profile',
+    icon: 'GroupsIcon',
+    calloutClass: 'rainbow',
+    items: [
+      {
+        label: 'Leaderboard',
+        href: 'http://localhost/teams',
+      },
+      {
+        label: 'Task Center',
+        href: 'http://localhost/profile/tasks',
+      },
+      {
+        label: 'Your Profile',
+        href: 'http://localhost/profile',
+      },
+    ],
+  },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'Overview',
+        href: 'https://pancakeswap.info',
+      },
+      {
+        label: 'Tokens',
+        href: 'https://pancakeswap.info/tokens',
+      },
+      {
+        label: 'Pairs',
+        href: 'https://pancakeswap.info/pairs',
+      },
+      {
+        label: 'Accounts',
+        href: 'https://pancakeswap.info/accounts',
+      },
+    ],
+  },
+  {
+    label: 'IFO',
+    icon: 'IfoIcon',
+    href: 'http://localhost/ifo',
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: 'Voting',
+        href: 'https://voting.pancakeswap.finance',
+      },
+      {
+        label: 'Github',
+        href: 'https://github.com/pancakeswap',
+      },
+      {
+        label: 'Docs',
+        href: 'https://docs.pancakeswap.finance',
+      },
+      {
+        label: 'Blog',
+        href: 'https://pancakeswap.medium.com',
+      },
+    ],
+  },
+]
+
+export default config
